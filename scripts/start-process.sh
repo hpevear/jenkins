4aba10426fdc7cff0548b06f2cc73b6fdcae5085
Hollis Pevear <hpevear@gmail.com>
2021-12-28 08:47:40 -0500

    Update start-process.sh

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo systemctl start MyJenkins
+# sudo systemctl start MyJenkins
+
+cd /home/ec2-user/server
+sudo /usr/bin/java -jar -Dserver.port=8081 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
